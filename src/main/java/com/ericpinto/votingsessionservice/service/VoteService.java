bc68dea308918d9a6cf6cdf91c0e76b3910e8379
ericgpinto <ericgrandopinto@gmail.com>
2024-12-25 19:58:07 -0300

    refactor(service): refatorar regra para votação

--- src/main/java/com/ericpinto/votingsessionservice/service/VoteService.java
+++ src/main/java/com/ericpinto/votingsessionservice/service/VoteService.java
@@ -9,9 +9,11 @@ import com.ericpinto.votingsessionservice.repository.AssociateRepository;
 import com.ericpinto.votingsessionservice.repository.VoteRepository;
 import com.ericpinto.votingsessionservice.request.VoteRequest;
 import com.ericpinto.votingsessionservice.response.VoteResponse;
+import com.fasterxml.jackson.annotation.OptBoolean;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class VoteService {
@@ -29,24 +31,31 @@ public class VoteService {
     }
 
     public VoteResponse vote(String idAgenda, String associateId, VoteRequest request) {
-        AgendaEntity agenda = agendaRepository.findById(idAgenda)
-                .orElseThrow(() -> new RuntimeException("Agenda not found"));
-
-       validate(agenda);
-
+        AgendaEntity agenda = agendaRepository.findById(idAgenda).orElseThrow(() -> new RuntimeException("Agenda not found"));
         AssociateEntity associate = associateRepository.findById(associateId).orElseThrow(() -> new RuntimeException("Associate not found"));
 
-        VoteEntity vote = VoteEntity.save(request, associate);
+        validateAgendaAndVote(agenda, associate);
+
+        VoteEntity vote = VoteEntity.create(request, associate);
+        vote = voteRepository.save(vote);
 
         agenda.getVoteEntity().add(vote);
         agendaRepository.save(agenda);
 
-       return VoteMapper.toResponse(voteRepository.save(vote));
+       return VoteMapper.toResponse(vote);
     }
 
-    private void validate(AgendaEntity agenda){
+    private void validateAgendaAndVote(AgendaEntity agenda, AssociateEntity associate){
         if (LocalDateTime.now().isAfter(agenda.getEndTime())){
-            throw new RuntimeException("Agenda end time is after start time");
+            throw new RuntimeException("Agenda is no longer open for voting");
+        }
+
+        boolean alreadyVoted = agenda.getVoteEntity().stream()
+                .anyMatch(vote -> vote.getAssociate().equals(associate));
+
+        if (alreadyVoted) {
+            throw new RuntimeException("Associate has already voted on this agenda");
         }
     }
+
 }
